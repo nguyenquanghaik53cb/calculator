ba774c3306e4c463a69cfd6ab8114e31b2480c8a
nguyenquanghaik53cb <nguyenquanghaik53cb@gmail.com>
2023-03-11 00:14:43 +0700

    upload source code for calculator application with reactjs

---
+++ src/components/Buttons.js
@@ -0,0 +1,135 @@
+import React from "react";
+import "./styles/Buttons.css";
+
+const Buttons = ({ inputHandler, clearInput, backspace, changePlusMinus, calculateAns }) => {
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      document.getElementById("equalbtn").click();
+    }
+  });
+
+  return (
+    <div className="show-btn">
+      <button className="btn exp" onClick={inputHandler}>
+        Rad
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        Deg
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        x!
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        sin
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        cos
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        tan
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        r
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        EXP
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        Ans
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        ^
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        e
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        (
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        )
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        √
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        x<sup>2</sup>
+      </button>
+      <button className="btn clr" onClick={clearInput}>
+        AC
+      </button>
+      <button className="btn clr" onClick={backspace}>
+        ⌫
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        log
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        ÷
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        %
+      </button>
+      <button className="btn" onClick={inputHandler}>
+        7
+      </button>
+      <button className="btn" onClick={inputHandler}>
+        8
+      </button>
+      <button className="btn" onClick={inputHandler}>
+        9
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        x
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        x<sup>3</sup>
+      </button>
+      <button className="btn" onClick={inputHandler}>
+        4
+      </button>
+      <button className="btn" onClick={inputHandler}>
+        5
+      </button>
+      <button className="btn" onClick={inputHandler}>
+        6
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        -
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        <sup>3</sup>√
+      </button>
+      <button className="btn" onClick={inputHandler}>
+        1
+      </button>
+      <button className="btn" onClick={inputHandler}>
+        2
+      </button>
+      <button className="btn" onClick={inputHandler}>
+        3
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        +
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        !
+      </button>
+      <button className="btn exp" onClick={changePlusMinus}>
+        ±
+      </button>
+      <button className="btn" onClick={inputHandler}>
+        0
+      </button>
+      <button className="btn exp" onClick={inputHandler}>
+        .
+      </button>
+      <button className="btn exp equal" id="equalbtn" onClick={calculateAns}>
+        =
+      </button>
+    </div>
+  );
+};
+
+export default Buttons;
